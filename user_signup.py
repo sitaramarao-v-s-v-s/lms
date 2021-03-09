2c76f1d232dfd24c1831ac4e01a2011b6851c221
srustith-anugandula <76683022+srustith-anugandula@users.noreply.github.com>
2021-03-09 10:12:30 +0530

    updated user sign_up

--- user_signup.py
+++ user_signup.py
@@ -3,25 +3,32 @@
 import base64
 import valid
 from connection import cursor , mydb
-
+from forgot_password import otp_generation,mail_generation,password_match
 def user_signup():
     """This module is used for user Signup
     """
     try:
         # Signup User Input
-        uid = valid.userid(input('enter user id : ').strip().lower())
+        user_id = valid.userid(input('enter user id : ').strip().lower())
         user_name = valid.user_name_validation(input('enter user name : ').strip())
         user_mail = valid.email_validation(input('enter user mail : ').strip().lower())
-        password =  valid.password_validation(input('enter password : ').strip())
         query = "select mail_id from user where user_id =%s or mail_id =%s;"
-        if cursor.execute(query, (uid,user_mail)):
+        if cursor.execute(query, (user_id,user_mail)):
             print('user already exists')
         else:
-            encoded_pwd = base64.b64encode(password.encode('ascii'))
-            query = "insert into user (user_id,user_name,mail_id,password) values(%s,%s,%s,%s)"
-            cursor.execute(query,(uid,user_name, user_mail, encoded_pwd))
-            mydb.commit()
-            print("User Creation Successful")
+            otp = otp_generation()
+            print(otp)
+            mail_generation(otp,user_mail)
+            check_otp = input('enter 6 digit otp sent your mail : ')
+            if check_otp == otp:
+                encoded_pwd = password_match()
+                encoded_pwd = base64.b64encode(password.encode('ascii'))
+                query = "insert into user (user_id,user_name,mail_id,password) values(%s,%s,%s,%s)"
+                cursor.execute(query,(user_id,user_name, user_mail, encoded_pwd))
+                mydb.commit()
+                print("User Creation Successful")
+            else:
+                print('incorrect OTP \n')                           
     except Exception as exception_e:
         print("Error",exception_e)
-  
+
